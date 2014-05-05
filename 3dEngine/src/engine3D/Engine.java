ea292777993756aafd2d20ad6823bdbb186e65f6
d-qoi <alex@d-qoi.com>
2014-05-05 09:26:09 -0500

    Beginning of rendering code...

--- 3dEngine/src/engine3D/Engine.java
+++ 3dEngine/src/engine3D/Engine.java
@@ -1,5 +1,7 @@
 package engine3D;
 
+import javax.annotation.processing.RoundEnvironment;
+
 public class Engine {
 	
 	private static int FOVAngleStep = 5;
@@ -73,15 +75,35 @@ public class Engine {
 		//TODO see if the entity intersects the FOVbox.
 		return false;
 	}
-	private RenderPolygon convertToScreenWorld(core.Bounds3D that) {
+	private RenderPolygon[] convertToScreenWorld(core.Bounds3D that) {
 		int[][][] faces = that.getFaces();
+		int[][] faceX;
+		int[][] faceY;
+		RenderPolygon[] polys = new RenderPolygon[2];
+		
+		if(camera.distanceToXY(faces[3][4][0], faces[3][4][1]) < camera.distanceToXY(faces[4][4][0], faces[4][4][1]))
+			faceX = faces[3];
+		else
+			faceX = faces[4];
+		
+		if(camera.distanceToXY(faces[1][4][0], faces[1][4][1]) < camera.distanceToXY(faces[2][4][0], faces[2][4][1]))
+			faceY = faces[1];
+		else
+			faceY = faces[2];
+		
+		for(int i = 0; i<faceX.length; i++)
+			faceX[i] = convertPointToScreen(faceX[i][0], faceX[i][1], faceX[i][2]);
+		
+		for(int i= 0; i<faceY.length; i++)
+			faceY[i] = convertPointToScreen(faceY[i][0], faceY[i][1], faceY[i][2]);
+		
+		//TODO if needed add one for Z, using the same math.
 		
 		
-		
-		return null;
+		return polys;
 	}
 	
-	private int[] convertPointtoScreen(int x, int y, int z) {
+	private int[] convertPointToScreen(int x, int y, int z) {
 		int[] point = new int[3];
 		point[0] = camera.getDeltaX(x) * (int)Math.sin(Math.toRadians(camera.getRotation()));
 		point[1] = camera.getDeltaY(y) * (int)Math.cos(Math.toRadians(camera.getRotation()));
