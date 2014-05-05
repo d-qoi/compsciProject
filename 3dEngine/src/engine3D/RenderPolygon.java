e8787e94699f0967088699bf4f335b88f981af06
d-qoi <alex@d-qoi.com>
2014-05-04 22:41:36 -0500

    New engine class, RenderPolygon.
    All it does is store the points to render

---
+++ 3dEngine/src/engine3D/RenderPolygon.java
@@ -0,0 +1,17 @@
+package engine3D;
+
+public class RenderPolygon {
+	public int x1,y1,x2,y2;
+
+	public RenderPolygon(int x1, int y1, int y2, int x2) {
+		this.x1 = x1;
+		this.y1 = y1;
+		this.y2 = y2;
+		this.x2 = x2;
+	}
+	
+	//TODO add other features
+	//This is what the engine will return for the game to render?
+	//TODO change as needed
+
+}
